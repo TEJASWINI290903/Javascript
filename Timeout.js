45672f863ece5a3c1652c7f1d0d9daf13eeb2c70
TEJASWINI290903 <727722euec193@skcet.ac.in>
2024-07-31 09:30:37 +0530

    first commit

---
+++ Timeout.js
@@ -0,0 +1,30 @@
+// SET TIMEOUT
+
+/*console.log("Set Timeout Example:");
+
+setTimeout(function(){
+    console.log("Hello Skcet!!");
+},1000);*/
+
+
+// SET INTERVAL
+
+/*console.log("Set TimeIntervalExample:");
+
+setInterval(function(){
+    console.log("Damon Salvatore");
+},2000)*/
+
+
+/*setInterval(() =>{
+    console.log("Welcome to setInterval");
+    console.log("Be patient code will execution");
+
+},1000);*/
+
+
+//SELF CALLING FUNCTION
+
+/*(function name(){
+    console.log("Good morning!!");
+})()*/
---
+++ callback.js
@@ -0,0 +1,36 @@
+/*function validation()
+{
+    console.log("Validation Successful");
+    completion();
+}
+function completion()
+{
+    console.log("Validation Completed");
+}
+validation()*/
+
+/*function validation(VC)
+{
+    console.log("Validation Successful");
+    VC();
+}
+function completion()
+{
+    console.log("Validation Completed");
+}
+validation(completion);*/
+
+
+//set timout function
+
+/*function validation(VC)
+{
+    setTimeout(()=>{
+    console.log("Validation Successful");
+    VC();},10000);
+}
+function completion()
+{
+    console.log("Validation Completed");
+}
+validation(completion);*/
